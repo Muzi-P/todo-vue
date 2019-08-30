d4379d9e31e9367f387523e45c73567470f0027a
Muzi-P <phiwokuhlegondwe@gmail.com>
2019-08-30 15:52:23 +0800

    Installing nad making UI

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+      <!-- <Login/> -->
+      <Signup/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    Signup
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container pa-12 >
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-form>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="blue">
+              <v-toolbar-title>Login</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+                <v-text-field 
+                    v-model="username" 
+                    prepend-icon="person" 
+                    name="login" 
+                    label="Login" 
+                    type="text">
+                </v-text-field>
+                <v-text-field 
+                    v-model="username" 
+                    prepend-icon="lock" 
+                    name="password" 
+                    label="Password" 
+                    type="password">
+                </v-text-field>
+                </v-card-text> 
+                    <v-divider light></v-divider>
+                <v-card-actions>
+                    <v-btn to="/signup" rounded color="indigo" dark>Sign up</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn rounded="" color="primary" dark @click.prevent="login()">
+                     Login
+                        <v-icon>keyboard_arrow_right</v-icon>
+                    </v-btn>
+            </v-card-actions>     
+          </v-card>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'login'
+};
+</script>
